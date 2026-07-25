14098d6a7045eba8772dfc0893172411008bf4f1
opdongaviota-wq <op.dongaviota@gmail.com>
2026-07-24 20:45:21 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service Worker · Inventario Barra Don Gaviota */
-const CACHE = 'barra-dg-v2';
+const CACHE = 'barra-dg-v3';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
